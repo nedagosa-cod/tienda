f2ae917f5e9c942157b05c13dee3f69139391e97
nedagosa-cod <nestorcode4004@gmail.com>
2022-05-15 13:51:09 -0500

    se avanza en el formulario login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,26 @@
 import './Login.css';
+import imgUser from '../img/user.png';
 
 const Login = () => {
     return (
         <>
-            <div class="input-container ic1">
-                <input id="firstname" class="input" type="text" placeholder=" " />
-                <div class="cut"></div>
-                <label for="firstname" class="placeholder">First name</label>
+        <div className='form'>
+            <h1 className='form__title'>Welcome</h1>
+            <figure className='form__figure'>
+                <img src={imgUser} alt="user" />
+            </figure>
+            <div className="form__bxInput">
+                <i className="fa-solid fa-user user__icon"></i>
+                <input id="email" className="form__bxInput--input" type="text" placeholder=" "/>
+                <label htmlFor="email" className="form__bxInput--label">e-mail</label>
             </div>
+            <div className="form__bxInput">
+                <i className="fa-solid fa-user user__icon"></i>
+                <input id="password" className="form__bxInput--input" type="password" placeholder=" " />
+                <label htmlFor="password" className="form__bxInput--label">Password</label>
+            </div>
+            <span className='form__span'><a href="#">Forgot Password</a></span>
+        </div>
         </>
     );
 }
